9f1794243b50e6e2cadb4b85895b4a1795ff7f2f
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2023-04-24 21:33:46 -0300

    Delete questao11.py

--- questao11.py
+++
@@ -1,7 +0,0 @@
-# Questão 11 - Crie uma lista vazia e peça para o usuário digitar 10 números. 
-# Em seguida, retorne os elementos de índice par da lista.
-
-lista = list()
-
-for i in range(10): lista.append(int(input(f'{i + 1}º valor: ')))
-for i in range(0, 10, 2): print(f'Valores na posição par: {lista[i]}')
