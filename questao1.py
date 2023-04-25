0765561031f6832ccdd3062c58a92a50cad9f466
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2023-04-24 21:40:19 -0300

    Delete questao1.py

--- questao1.py
+++
@@ -1,9 +0,0 @@
-# Questão 1 - Peça para o usuário digitar 5 números e crie uma lista com esses números. 
-# Em seguida, peça para o usuário digitar mais um número e adicione esse número à lista.
-
-lista = list()
-
-for i in range(5): lista.append((int(input(f'{i + 1}º valor: '))))
-
-lista.append(int(input('Insira o 6º valor: ')))
-print(lista)
